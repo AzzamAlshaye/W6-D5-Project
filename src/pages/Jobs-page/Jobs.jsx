755fc71717c4b272cc478e756862c792d146d46f
AzzamAlshaye <azzamwow@hotmail.com>
2025-05-24 14:19:49 +0300

    section2-p3 done

--- src/pages/Jobs-page/Jobs.jsx
+++ src/pages/Jobs-page/Jobs.jsx
@@ -2,11 +2,13 @@
 import React from "react";
 import { FaUser, FaEnvelope, FaComment, FaPaperPlane } from "react-icons/fa";
 import VideoSection from "./Video-Section";
+import StartCareer from "./StartCareer";
 
 export default function ContactUs() {
   return (
     <div className="flex flex-col bg-black">
       <VideoSection />
+      <StartCareer />
     </div>
   );
 }
---
+++ src/pages/Jobs-page/StartCareer.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+
+const categories = [
+  {
+    imageSrc:
+      "https://cdn.phenompeople.com/CareerConnectResources/ACPUUS/images/EVPIconssubitem11-1710455685013.png",
+    href: "#",
+  },
+  {
+    imageSrc:
+      "https://cdn.phenompeople.com/CareerConnectResources/ACPUUS/images/EVPIconssubitem12-1710455727924.png",
+    href: "#",
+  },
+  {
+    imageSrc:
+      "https://cdn.phenompeople.com/CareerConnectResources/ACPUUS/images/EVPIconssubitem13-1710455855824.png",
+    href: "#",
+  },
+  {
+    imageSrc:
+      "https://cdn.phenompeople.com/CareerConnectResources/ACPUUS/images/EVPIconssubitem15-1710456129634.png",
+    href: "#",
+  },
+  {
+    imageSrc:
+      "https://cdn.phenompeople.com/CareerConnectResources/ACPUUS/images/EVPIconssubitem16-1710456257523.png",
+    href: "#",
+  },
+  {
+    imageSrc:
+      "https://cdn.phenompeople.com/CareerConnectResources/ACPUUS/images/EVPIconssubitem17-1710456307621.png",
+    href: "#",
+  },
+  {
+    imageSrc:
+      "https://cdn.phenompeople.com/CareerConnectResources/ACPUUS/images/EVPIconssubitem18-1710456364531.png",
+    href: "#",
+  },
+  {
+    imageSrc:
+      "https://cdn.phenompeople.com/CareerConnectResources/ACPUUS/images/EVPIconssubitem19-1710456408214.png",
+    href: "#",
+  },
+  {
+    imageSrc:
+      "https://cdn.phenompeople.com/CareerConnectResources/ACPUUS/images/EVPIconssubitem14-1710456001039.png",
+    href: "#",
+  },
+];
+
+export default function StartCareer() {
+  return (
+    <section id="start-career" className="bg-black text-white py-16">
+      <div className="max-w-7xl mx-auto px-4">
+        <h2 className="text-3xl lg:text-4xl font-bold text-center mb-8">
+          START YOUR GAME CHANGING CAREER
+        </h2>
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+          {categories.map((cat, idx) => (
+            <a
+              key={idx}
+              href={cat.href}
+              className="flex items-center justify-center p-4 hover:opacity-80 transition-opacity"
+            >
+              <img
+                src={cat.imageSrc}
+                alt={`Category ${idx + 1}`}
+                className="h-18 w-auto object-contain"
+              />
+            </a>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
